daf28fa78b71bcd6ea2c6db07dfc00c663f3998f
Nisita Chandel <nisitachandel@gmail.com>
2026-02-11 22:22:24 +0530

    updated code

--- main.js
+++ main.js
@@ -6,33 +6,40 @@ const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeig
 camera.position.z = 5;
 
 const cubegeo = new THREE.BoxGeometry(1,1,1)
-const cubemat = new THREE.MeshBasicMaterial({color:"red"});
+const cubemat = new THREE.MeshBasicMaterial({color:"red" , wireframe:true});
 const cube = new THREE.Mesh(cubegeo,cubemat);
 
-cube.position.x = -1;
+cube.position.x = 0.1;
 
 
-const spheregeo = new THREE.SphereGeometry(1,10,10)
-const spheremat = new THREE.MeshBasicMaterial({color:"red",wireframe:true});
-const sphere = new THREE.Mesh(spheregeo,spheremat);
+scene.add(cube);
 
-sphere.position.x = 1;
-const group = new THREE.Group();
-group.add(cube);
-group.add(sphere);
 
-group.position.x = -2;
+// const spheregeo = new THREE.SphereGeometry(1,10,10)
+// const spheremat = new THREE.MeshBasicMaterial({color:"red",wireframe:true});
+// const sphere = new THREE.Mesh(spheregeo,spheremat);
 
-scene.add(group)
+// sphere.position.x = 1;
+// const group = new THREE.Group();
+// group.add(cube);
+// group.add(sphere);
+
+// group.position.x = -2;
+
+// scene.add(group)
 
 const canvas = document.querySelector("canvas");
 const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize(window.innerWidth,window.innerHeight);
 
-renderer.render(scene,camera);
+let clock = new THREE.Clock();
 
-// function animate(){
-//     window.requestAnimationFrame(animate);
-//     mesh.rotation.y += 0.1;
-// }
-// animate();
+
+function animate(){
+    window.requestAnimationFrame(animate);
+    cube.rotation.y = clock.getElapsedTime()*6;
+    // cube.rotation.y += 0.1;
+    renderer.render(scene,camera);
+    // mesh.rotation.y += 0.1;
+}
+animate();
